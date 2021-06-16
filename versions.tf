20a1f682d0673ed317770fb635b9083766c63319
Andrés Muñoz <andresmvera@gmail.com>
2021-06-16 15:37:08 +0200

    SYS-1881 Adding to versions.tf the required_providers block with configuration_aliases to name the providers

--- versions.tf
+++ versions.tf
@@ -1,4 +1,10 @@
 
 terraform {
   required_version = ">= 0.12"
+  required_providers {
+    aws = {
+      version = "~> 3.0"
+      configuration_aliases = [ aws.requester, aws.accepter ]
+    }
+  }
 }
